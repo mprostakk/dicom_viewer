e43a3abd066c8170b3bb59851ebbd8f8bdbdcc6d
xszym <xszym69@gmail.com>
2020-10-16 13:36:48 +0200

    Code refactor

--- dicom_app/dicom_reader.py
+++ dicom_app/dicom_reader.py
@@ -1,16 +1,6 @@
 import pydicom
+
 from pydicom.data import get_testdata_files
-
-from PIL import Image
-from PyQt5.QtGui import QImage
-import matplotlib.pyplot as plt
-
-
-def convert_dicom_to_QImage(cv_img):
-    height, width = cv_img.shape
-    bytes_per_line = 3 * width
-    q_img = QImage(cv_img.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
-    return q_img
 
 
 def get_dicom(filename='CT_small.dcm'):
--- gui/main_window.py
+++ gui/main_window.py
@@ -1,59 +1,29 @@
-import sys
 import logging
-import numpy as np
-
-
-from PyQt5 import QtGui
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QAction, QLabel, QScrollArea, QHBoxLayout, QMenu, QWidget, QSizePolicy, QApplication, QMainWindow, QSlider, QHBoxLayout
-
-from matplotlib.backends.backend_qt5agg import FigureCanvas
 
 from dicom_app import dicom_reader
+from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.figure import Figure
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QAction, QLabel, QMenu, QWidget, QMainWindow, \
+    QSlider, QHBoxLayout
+
 
 class DicomViewer(QMainWindow):
-
     file_menu: QMenu
-
     test_action: QAction
     action_exit: QAction
 
     def __init__(self):
         super(DicomViewer, self).__init__()
         self.test_dicom = dicom_reader.get_dicom()
-        self.initUI()
+        self.init_ui()
 
-    def initUI(self):
-
-        layout = QHBoxLayout()
-
-        # slider
-        sld = QSlider(Qt.Vertical, self)
-        sld.setGeometry(40, 30, 30, 200)
-        sld.setRange(0, 100) # len(self.test_dicom)
-        sld.valueChanged[int].connect(self.change_value)
-        layout.addWidget(sld)
-
-        # Label with number
-        self.label = QLabel("0", self)
-        self.label.setStyleSheet('QLabel { background: #007AA5; border-radius: 3px;}')
-        self.label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
-        self.label.setMinimumWidth(80)
-        layout.addWidget(self.label)
-
-        # image
-        self.figure = Figure(figsize=(5, 3))
-        self.canvas = FigureCanvas(self.figure)
-        self.ax = self.figure.subplots()
-        self.ax.imshow(dicom_reader.get_image_from_dicom(self.test_dicom, 0))
-        self.ax.set_axis_off()
-        layout.addWidget(self.canvas)
-
-        # widget
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+    def init_ui(self):
+        self.layout = QHBoxLayout()
+        self.init_slider()
+        self.init_label()
+        self.init_image()
+        self.init_widget()
 
         self.create_actions()
         self.create_menu()
@@ -62,12 +32,36 @@ class DicomViewer(QMainWindow):
         self.setMinimumWidth(800)
         self.setMinimumHeight(600)
 
+    def init_slider(self):
+        slider = QSlider(Qt.Vertical, self)
+        slider.setGeometry(40, 30, 30, 200)
+        slider.setRange(0, 100) # len(self.test_dicom)
+        slider.valueChanged[int].connect(self.change_value)
+        self.layout.addWidget(slider)
+
+    def init_label(self):
+        self.label = QLabel("0", self)
+        self.label.setStyleSheet(
+            'QLabel { background: #007AA5; border-radius: 3px;}')
+        self.label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        self.label.setMinimumWidth(80)
+        self.layout.addWidget(self.label)
+
+    def init_image(self):
+        self.figure = Figure(figsize=(5, 3))
+        self.canvas = FigureCanvas(self.figure)
+        self.ax = self.figure.subplots()
+        self.ax.imshow(dicom_reader.get_image_from_dicom(self.test_dicom, 0))
+        self.ax.set_axis_off()
+        self.layout.addWidget(self.canvas)
+
+    def init_widget(self):
+        widget = QWidget()
+        widget.setLayout(self.layout)
+        self.setCentralWidget(widget)
+
     def change_value(self, value):
         self.label.setText(str(value))
-
-    def create_actions(self):
-        self.test_action = QAction("&Test...", self, shortcut="Ctrl+T", triggered=self.test)
-        self.action_exit = QAction("&Exit...", self, shortcut="Alt+F4", triggered=self.on_exit)
 
     def create_menu(self):
         self.file_menu = QMenu("&File", self)
@@ -75,6 +69,12 @@ class DicomViewer(QMainWindow):
         self.file_menu.addSeparator()
         self.file_menu.addAction(self.action_exit)
         self.menuBar().addMenu(self.file_menu)
+
+    def create_actions(self):
+        self.test_action = QAction(
+            "&Test...", self, shortcut="Ctrl+T", triggered=self.test)
+        self.action_exit = QAction(
+            "&Exit...", self, shortcut="Alt+F4", triggered=self.on_exit)
 
     def test(self):
         logging.info('Test')
