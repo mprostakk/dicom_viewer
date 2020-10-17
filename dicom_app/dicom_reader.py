d7c4737e0ca087533e91543ec22c1984b8ee681d
Norbert Ozga <norbert.ozga@cervirobotics.com>
2020-10-17 13:31:14 +0200

    Implement 3d image creation from 2d images list

--- dicom_app/dicom_reader.py
+++ dicom_app/dicom_reader.py
@@ -12,9 +12,17 @@ from pydicom.data import get_testdata_files
 
         return files
 
+    def create_3d_array(self, slices: tp.List) -> None:
+        # calculate pixel aspects
+        pixel_spacing = slices[0].PixelSpacing
+        slice_thickness = slices[0].SliceThickness
+        self.ax_aspect = pixel_spacing[1] / pixel_spacing[0]
+        self.sag_aspect = pixel_spacing[1] / slice_thickness
+        self.cor_aspect = slice_thickness / pixel_spacing[0]
 
-def get_image_from_dicom(dicom_files, index):
-    file = dicom_files[index]
-    dataset = pydicom.dcmread(file)
-    arr = dataset.pixel_array
-    return arr
+        # create 3D array
+        self.image_shape = list(slices[0].pixel_array.shape)
+        self.image_shape.append(len(slices))
+        self.image_3d = np.zeros(self.image_shape)
+        for i, slice in enumerate(slices):
+            self.image_3d[:, :, i] = slice.pixel_array
