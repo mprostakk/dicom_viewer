223d898838163753d375c9feae262d26a34691d8
Norbert Ozga <norbert.ozga@cervirobotics.com>
2020-10-17 13:30:47 +0200

    Implement loading dicom directory

--- dicom_app/dicom_reader.py
+++ dicom_app/dicom_reader.py
@@ -1,10 +1,16 @@
 import pydicom
 from pydicom.data import get_testdata_files
 
+    def load_dicom_files(self, directory: str) -> tp.List[pydicom.FileDataset]:
+        files = []
+        for file_name in glob.glob(directory, recursive=False):
+            logging.info(file_name)
+            files.append(pydicom.dcmread(file_name))
 
-def get_dicom(filename='CT_small.dcm'):
-    files = get_testdata_files(filename)
-    return files
+        files = list(filter(lambda file: hasattr(file, 'SliceLocation'), files))
+        files = list(sorted(files, key=lambda file: file.SliceLocation))
+
+        return files
 
 
 def get_image_from_dicom(dicom_files, index):
